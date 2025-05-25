d216f61398b53753e0bbcc4dd18a9adf6f0fc5c8
unknown <yormsanti38@gimail.com>
2025-05-25 16:19:29 +0700

    encapsulation

---
+++ subclass.java
@@ -0,0 +1,12 @@
+public class subclass {
+    private String name ;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String newname) {
+        this.name = newname;
+    }
+    
+    
+}
