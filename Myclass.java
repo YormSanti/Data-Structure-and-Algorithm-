91cfa8e610b1faa2bac133a76a583faf7ab29c93
unknown <yormsanti38@gimail.com>
2025-05-25 16:23:22 +0700

    Encapsulation

---
+++ Myclass.java
@@ -0,0 +1,15 @@
+
+
+
+
+public class Myclass {
+    public static void main(String[] args) {
+        subclass obj = new subclass();
+        obj.setName("");
+        System.out.println("Name: " + obj.getName());
+        
+    }
+ 
+    
+    
+}
