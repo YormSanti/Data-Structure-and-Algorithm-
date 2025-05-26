1f2ed5a1cb89506ebfd3dc2f2ca120e2d17ce181
unknown <yormsanti38@gimail.com>
2025-05-26 19:14:11 +0700

    DaS

---
+++ List_O.java
@@ -0,0 +1,25 @@
+
+public class List_O {
+    public static void main(String[] args) {
+        int[] arr = {4,2,1,3,5,6,7};
+        int[] newArr = new int[arr.length - 1];
+        for (int i = 1; i < arr.length; i++) {
+            newArr[i - 1] = arr[i];
+  
+        }
+        int newNum = 8;
+        newArr[newArr.length - 1] = newNum;
+        //after add
+
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.print(newArr[i] + " ");
+        }
+        System.out.println(); 
+       
+
+        
+    }
+   
+
+
+}
