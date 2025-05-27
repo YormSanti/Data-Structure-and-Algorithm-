f41aafa3e1f8097cba72dc4f781cfb1e8ce2e577
unknown <yormsanti38@gimail.com>
2025-05-27 20:27:10 +0700

    Package

---
+++ List_1.java
@@ -0,0 +1,18 @@
+
+
+public class List_1 {
+
+    public static void main(String[] args) {
+        
+        Car myFastCar = new Car();
+        myFastCar.setName("Mustang");
+        myFastCar.honk();
+        System.out.println(myFastCar.brand + " " + myFastCar.getName());
+        List_O.arr();
+
+
+        
+
+        
+    }
+}
