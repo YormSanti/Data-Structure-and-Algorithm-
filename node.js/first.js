5b6541d1c89a23c85b621145b70a108f1d266bc8
unknown <yormsanti38@gimail.com>
2025-06-02 14:20:03 +0700

    dsa week three

---
+++ node.js/first.js
@@ -0,0 +1,9 @@
+
+console.log("Hello js");
+
+var num = [3,1,2,4];
+console.log(num);
+
+for(i =0; i < num.length;i++){
+    console.log('index = ' + i + " has " + num[i])
+}
---
+++ php/first.php
@@ -0,0 +1,13 @@
+<?php
+
+$num = [3,1,2,4];
+print_r($num);
+
+foreach( $num as $item ) {
+    echo"Item = $item\n";
+}
+?>
+
+
+
+
